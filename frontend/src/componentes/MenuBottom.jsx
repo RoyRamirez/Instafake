3d8c377c97b3102a389a1854119bb394a622026b
RoyRamirez <royramlo13@gmail.com>
2021-08-27 10:29:31 -0500

    Se agregó menú inferior

--- frontend/src/componentes/MenuBottom.jsx
+++ frontend/src/componentes/MenuBottom.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { FaSearch, FaRegUserCircle, FaShoppingBag, FaHome } from 'react-icons/fa';
+import { BsCollectionPlayFill } from 'react-icons/bs';
+import './styles/MenuBottom.css'
 
 export const MenuBottom = () => {
   return (
-    <div>
-      
+    <div className="menuBottom">
+      <a href="/" className="icon-bottom"><FaHome /></a>
+      <a href="/" className="icon-bottom"><FaSearch /></a>
+      <a href="/" className="icon-bottom"><BsCollectionPlayFill /></a>
+      <a href="/" className="icon-bottom"><FaShoppingBag /></a>
+      <a href="/" className="icon-bottom"><FaRegUserCircle /></a>
     </div>
   )
 }
