d7784ad1d8a3bec77cd544847eb9e6128a74ffcb
RoyRamirez <royramlo13@gmail.com>
2021-08-27 11:35:12 -0500

    Se agregó menú superior

--- frontend/src/componentes/MenuTop.jsx
+++ frontend/src/componentes/MenuTop.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { FcLike } from 'react-icons/fc';
+import { RiMessengerLine } from 'react-icons/ri';
+import { CgAddR } from 'react-icons/cg';
+import logo from '../imgs/logo.png';
+import './styles/MenuTop.css';
 
 export const MenuTop = () => {
   return (
-    <div>
-      
+    <div className="menuTop">
+      <img src={logo} alt="" className="logo" />
+      <div className="leftMenu">
+      <a href="/" className="icon-top"><CgAddR /></a>
+      <a href="/" className="icon-top"><FcLike /></a>
+      <a href="/" className="icon-top"><RiMessengerLine /></a>
+      </div>
     </div>
   )
 }
