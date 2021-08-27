40c958d6bf989cdfb71907055f174ca981e5f3c0
RoyRamirez <royramlo13@gmail.com>
2021-08-27 15:58:16 -0500

    Se agregaron las historias

--- frontend/src/componentes/Historias.jsx
+++ frontend/src/componentes/Historias.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import userPic from '../imgs/user.png';
+import './styles/Historias.css';
 
 export const Historias = () => {
   return (
-    <div>
-      
+    <div className="historias">
+        <a href="/" className="imgRef"><img src={userPic} alt="" className="historia" /></a>
+        <a href="/" className="imgRef"><img src={userPic} alt="" className="historia" /></a>
+        <a href="/" className="imgRef"><img src={userPic} alt="" className="historia" /></a>
+        <a href="/" className="imgRef"><img src={userPic} alt="" className="historia" /></a>
+        <a href="/" className="imgRef"><img src={userPic} alt="" className="historia" /></a>
     </div>
   )
 }
